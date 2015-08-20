c444df50b636ff0f570b9b90e779b664f83b5131
muit <miguel_3c@hotmail.com>
2015-08-20 21:27:17 +0200

    Setup Project

---
+++ Assets/Source/Engine/Character.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets._2D;
+
+[RequireComponent(typeof(PlatformerCharacter2D))]
+
+public class Character : MonoBehaviour {
+
+	void Start () {}
+	
+	void Update () {}
+}
---
+++ Assets/Source/Engine/SceneScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class SceneScript : MonoBehaviour
+{
+    // Static singleton property
+    public static SceneScript Instance { get; private set; }
+
+    void Awake()
+    {
+        // Check if there are any other instances conflicting
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
+
+        //DontDestroyOnLoad(gameObject);
+    }
+
+
+
+    void Start() {
+        CPlayer player = GameObject.FindObjectOfType<CPlayer>();
+        OnGameStart(player);
+    }
+
+    protected virtual void OnGameStart(CPlayer player) {}
+}
